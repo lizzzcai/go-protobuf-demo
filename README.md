041863d047699fa0f423aa733c81fc75e2bb6647
lizzzcai <lilcolinn@gmail.com>
2019-12-06 16:28:59 +0800

    add server, update dependency in readme

--- README.md
+++ README.md
@@ -6,3 +6,4 @@
 1. Install the standard C++ implementation of protocol buffers from https://developers.google.com/protocol-buffers/
 2. Install the Go compiler and tools from https://golang.org/ See https://golang.org/doc/install
 3. Install the `proto` package by `go get -u github.com/golang/protobuf/protoc-gen-go`
+4. gorilla/mux for REST endpoint `go get -u github.com/gorilla/mux`
---
+++ server/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/golang/protobuf/proto"
+	"github.com/gorilla/mux"
+	"github.com/lizzzcai/go-protobuf-demo/proto/echo"
+)
+
+func Echo(writer http.ResponseWriter, req *http.Request) {
+	contentLength := req.ContentLength
+	log.Printf("Content Length Received: %v\n", contentLength)
+	request := &echo.EchoRequest{}
+	data, err := ioutil.ReadAll(req.Body)
+	if err != nil {
+		log.Fatalf("Unable to read message from request : %v", err)
+	}
+
+	err = proto.Unmarshal(data, request)
+	if err != nil {
+		log.Fatalf("Unable to un-marshal the message : %v", err)
+	}
+
+	name := request.GetName()
+	responseContent := &echo.EchoResponse{Message: fmt.Sprintf("Hello %s", name)}
+	response, err := proto.Marshal(responseContent)
+	if err != nil {
+		log.Fatalf("Unable to marshal response : %v", err)
+	}
+	writer.Write(response)
+}
+
+func main() {
+	log.Println("Starting the API server...")
+	r := mux.NewRouter()
+	r.HandleFunc("/echo", Echo).Methods("POST")
+
+	server := &http.Server{
+		Handler: r,
+		Addr: "0.0.0.0:8000",
+		WriteTimeout: 2 * time.Second,
+		ReadTimeout: 2 * time.Second,
+	}
+
+	log.Fatal(server.ListenAndServe())
+}
